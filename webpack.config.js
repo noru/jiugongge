160303e4df95745e5ae58e384805d3f11eb69f4f
xiuzhu <nooooru@gmail.com>
2017-05-17 23:32:47 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: "jiugongge.js",
+    filename: "index.js",
     library: ["jiugongge"],
     libraryTarget: "umd"
   },
